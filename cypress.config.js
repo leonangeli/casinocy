bfe6eab4ebb20ff0ba382d06f93887cfdd6b5e18
leonangeli.agustin <leonangeli.agustin@gmail.com>
2024-03-05 04:20:31 -0300

    extended pageLoadTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  pageLoadTimeout: 120000,
   chromeWebSecurity: false,
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'Demo Casino Cypress Report',
+    reportPageTitle: "Demo Casino Cypress Report",
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
   },
   e2e: {
-    baseUrl:'https://demo.casino/',
+    baseUrl: "https://demo.casino/",
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
